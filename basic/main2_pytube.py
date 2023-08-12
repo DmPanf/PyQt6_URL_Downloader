841ceb9a060258648abb5e15670bcee76fa284d8
Dmitrii Panfilov <99917230+DmPanf@users.noreply.github.com>
2023-08-12 21:58:23 +0300

    Create main2_pytube.py

---
+++ basic/main2_pytube.py
@@ -0,0 +1,12 @@
+# PyTube (https://pytube.io/en/latest/) — для быстрой загрузки видео с YouTube
+from pytube import YouTube
+
+def download_best_mp4_video(link):
+    yt = YouTube(link)
+    best_video = yt.streams.filter(progressive=True, file_extension='mp4') \
+        .order_by('resolution').desc().first()
+    best_video.download()
+
+my_link = 'https://youtu.be/GON_7_Yfc1A'
+#YouTube(my_link).streams.first().download() # .3gpp
+download_best_mp4_video(my_link)
