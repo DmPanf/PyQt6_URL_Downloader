47f0e51bd506880d0ab72f1a9491f23076aaae48
Dmitrii Panfilov <99917230+DmPanf@users.noreply.github.com>
2023-08-12 22:00:22 +0300

    Update main3_youtube_dl.py

--- basic/main3_youtube_dl.py
+++ basic/main3_youtube_dl.py
@@ -3,4 +3,4 @@ import youtube_dl
 ydl_opts = {'outtmpl': './video/%(title)s.%(ext)s'}
 
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(['https://youtu.be/vWq-mIxvH3k'])
+    ydl.download(['https://youtu.be/...'])
